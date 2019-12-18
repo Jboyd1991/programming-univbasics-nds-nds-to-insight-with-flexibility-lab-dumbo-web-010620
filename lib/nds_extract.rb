e5d5fab7e5204836dbc0551796524c03df20dd5f
Julianna Boyd <julianna.boyd91@gmail.com>
2019-12-18 20:54:15 +0000

    Automatically backed up by Learn

--- lib/nds_extract.rb
+++ lib/nds_extract.rb
@@ -36,7 +36,7 @@ end
 def movies_with_director_key(name, movies_collection)
   dir_name = "Byron Poodle"
   updated_movies = movies_with_director_key(name, movies_collection)
-  updated_movies = [:director_name].push 
+  updated_movies = [:director_name][0].push 
     
   
   #GOAL: For each Hash in an Array (movies_collection), provide a collection
